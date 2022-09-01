362a686a40891bdea143485d1fe43f589999e569
frontlich <frontlich@163.com>
2022-09-01 13:52:15 +0800

    HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,13 @@
 import { lazy, Suspense } from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 
 import "./index.css";
 
 const Demo1 = lazy(() => import("./pages/demo1"));
 
 const App = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route
         path="demo1"
@@ -20,7 +20,7 @@ const App = () => (
       <Route index element={<Navigate to="demo1" />} />
       <Route path="*" element={<div>该页面不存在</div>} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
 
 const root = ReactDOM.createRoot(
